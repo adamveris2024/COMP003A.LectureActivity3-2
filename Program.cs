f373c608939c3bc7f0af1b116429417390fc2a2d
adamveris2024 <adamveris2024@gmail.com>
2025-01-27 17:57:07 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.LectureActivity3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
